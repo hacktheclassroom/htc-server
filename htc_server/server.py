2751f267c4a6d0c85ae4c66c02b6d2fef40d41bc
vesche <austin.jackson@swimlane.com>
2019-02-23 17:19:12 -0700

    Update server

--- htc_server/server.py
+++ htc_server/server.py
@@ -12,17 +12,60 @@ from common import HTCServerInvalidPayload
 app = Sanic()
 
 
-@app.route('/solve', methods=['POST'])
+def validate_server_code(server_code):
+    """Validate the server code."""
+    raise NotImplementedError
+
+
+def solve_check(level_id, flag):
+    """Check if the Flag is correct for a given Level ID."""
+    raise NotImplementedError
+
+
+def score_lookup(username):
+    """Get the score of a player."""
+    raise NotImplementedError
+
+
+@app.route('/validate', methods=['GET', ])
+async def validate(request):
+    """Validate server code."""
+
+    try:
+        server_code = request.json['server_code']
+    except KeyError:
+        raise HTCServerInvalidPayload
+
+    # TODO: validate server code
+    # validate_server_code(server_code)
+
+    return response.json({'success': True})
+
+
+@app.route('/solve', methods=['GET', ])
 async def solve(request):
+    """Attempt to solve a puzzle."""
+
     try:
         level_id = request.json['level_id']
         flag = request.json['flag']
     except KeyError:
         raise HTCServerInvalidPayload
 
-    # check Level ID and Flag here :)
+    # TODO: check Level ID and Flag here :)
+    # solve_check(level_id, flag)
 
     return response.json({'success': True})
+
+
+@app.route('/score/<username>', methods=['GET', ])
+async def get_score(request, username):
+    """Get the score of a player given a username."""
+
+    # TODO: get player score
+    # score_lookup(username)
+
+    return response.json({'score': 100})
 
 
 if __name__ == '__main__':
